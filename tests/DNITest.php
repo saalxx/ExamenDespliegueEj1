e70e031a9faa61495147399e46a206cccdd680fb
saalxx <saalxx@alumnatflorida.es>
2026-02-03 11:10:25 +0100

    cuarto commit

--- tests/DNITest.php
+++ tests/DNITest.php
@@ -1,10 +1,10 @@
 <?php
-
+use SebastianBergmann\Environment\Console;
 use PHPUnit\Framework\TestCase;
 use App\DNI;
 class DNITest extends TestCase
 {
-    public function getDNIConletra()
+    public function testSuma()
     {
         $calc = new DNI(12345678);
         $this->assertNotNull($calc->getDNIConLetra());
